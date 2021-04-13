c0b0dcedd0fc95d33f3f4bc75662419415192f84
zhuyouzheng <44113448+zuryazen@users.noreply.github.com>
2021-04-13 20:20:49 +0800

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,6 @@
 package com.zhuyz.algorithm.stack;
 
 
-
 /**
  * 3+2*6-2
  *
@@ -11,20 +10,24 @@ package com.zhuyz.algorithm.stack;
  *      如果运算符栈为空，直接压入
  *      如果运算符栈有值，则比较当前字符<==>栈中运算符的优先级：
  *          <=：取出运算符栈字符和操作数栈中的两个操作数进行计算，计算完的结果重新压入操作数栈
- *          >：将当前字符压入运算符栈
- * 4.循环结束，顺序从操作数栈和运算符栈中pop出相应的操作数和运算符进行运算
- * 5.最后数栈只有一个数字，就是结果。
- *
+ *          >：将当前字符压入运算符栈 4.循环结束，顺序从操作数栈和运算符栈中pop出相应的操作数和运算符进行运算 5.最后数栈只有一个数字，就是结果。
  */
 public class Calculator {
 
     public static void main(String[] args) {
-        String expr = "3+2*6-2";
-        String expr2 = "3+2*6-2+8/2*3-3+1*3";
-        int res = calulate(expr);
-        int res2 = calulate(expr2);
-        System.out.println(res);
-        System.out.println(res2);
+        //String expr = "3+2*6-2";
+        //String expr2 = "3+2*6-2+8/2*3-3+1*3";
+        // 实现多位数字的四则运算
+        String expr3 = "70+2*6-4";
+        String expr4 = "70+2*60-4";
+        //int res = calulate(expr);
+        //int res2 = calulate(expr2);
+        int res3 = calulate(expr3);
+        int res4 = calulate(expr4);
+        //System.out.println(res);
+        //System.out.println(res2);
+        System.out.println(res3);
+        System.out.println(res4);
     }
 
     public static int calulate(String expr) {
@@ -35,7 +38,23 @@ public class Calculator {
             boolean oper = ArrayStack.isOper(c);
             if (!oper) {
                 // 操作数
-                numStack.push(Integer.valueOf(String.valueOf(c)));
+                StringBuilder sb = new StringBuilder();
+                sb.append(c);
+                // 个位数没有问题，但是多位数的情况下会出现问题
+                while (true) {
+                    // 如果下标越界，直接break
+                    if (i + 1 >= expr.length()){
+                        break;
+                    }
+                    // 如果下一位数是运算符，就break；
+                    if (ArrayStack.isOper(expr.charAt(i + 1))) {
+                        break;
+                    } else {
+                        // 如果还是数字就拼接
+                        sb.append(expr.charAt(++i));
+                    }
+                }
+                numStack.push(Integer.valueOf(sb.toString()));
             } else {
                 // 运算符
                 // 1.判断运算符栈是否有值
@@ -60,9 +79,8 @@ public class Calculator {
                 }
             }
         }
-        System.out.println();
         int len = operStack.top + 1;
-        for (int i = 0; i <len; i++) {
+        for (int i = 0; i < len; i++) {
             int next = numStack.pop();
             int prev = numStack.pop();
             int pop = operStack.pop();
@@ -72,9 +90,9 @@ public class Calculator {
     }
 
 
-
     // 扩展功能
     public static class ArrayStack {
+
         private int maxSize; // 栈大小
         private int[] arr;
         private int top = -1; // 表示栈顶
@@ -153,8 +171,5 @@ public class Calculator {
             }
             return res;
         }
-
-
     }
-
 }
