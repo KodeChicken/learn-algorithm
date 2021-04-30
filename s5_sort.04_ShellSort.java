f106eae2535f2f3093e3c773f091506463e79f9a
zhuyouzheng <44113448+zuryazen@users.noreply.github.com>
2021-04-30 10:40:10 +0800

    Update s5_sort.04_ShellSort.java

--- s5_sort.04_ShellSort.java
+++ s5_sort.04_ShellSort.java
@@ -70,24 +70,28 @@ public class ShellSort {
         int[] arr = new int[]{8, 9, 1, 7, 2, 3, 5, 4, 6, 0};
         //int[] arr = new int[]{1, 3, 11, 17, 22, 13, 15, 9, 6, 0};
 
-        // 第1轮
+        // 第1轮排序，是将10个数据分成了5组
         for (int i = 5; i < arr.length; i++) {
+            // 遍历各组中所有的元素(共5组，每组有2个元素)，步长: 5
             for (int j = i - 5; j >= 0; j -= 5) {
+                // 如果当前元素大于加上步长的那个元素，说明需要交换
                 exchange(arr, j, 5);
             }
         }
         System.out.println("第1轮排序：" + Arrays.toString(arr));
 
-        // 第2轮
+        // 第2轮排序，是将10个数据分成了2组
         for (int i = 2; i < arr.length; i++) {
+            // 遍历各组中所有的元素(共2组，每组有5个元素)，步长: 2
             for (int j = i - 2; j >= 0; j -= 2) {
                 exchange(arr, j, 2);
             }
         }
         System.out.println("第2轮排序：" + Arrays.toString(arr));
 
-        // 第3轮
+        // 第3轮排序，是将10个数据分成了1组
         for (int i = 1; i < arr.length; i++) {
+            // 遍历各组中所有的元素(共1组，每组有10个元素)，步长: 1
             for (int j = i - 1; j >= 0; j -= 1) {
                 exchange(arr, j, 1);
             }
